3afa935c0527fb25b35b21619d1d30bd9243f82d
juaantoniv <juraantoniv@gmail.com>
2022-10-09 17:52:41 +0300

    ok

--- src/services/Axios.service.js
+++ src/services/Axios.service.js
@@ -1,8 +1,18 @@
 import axios from "axios";
 
 import {baseURL} from "../configs";
+import {authService} from "./auth.service";
 
 const axiosService = axios.create({baseURL})
+
+axiosService.interceptors.request.use((config)=>{
+    const access = authService.getAccessToken()
+
+    if (access){
+        config.headers.Authorization = `Bearer${access}`
+    }
+    return config
+})
 
 export {
     axiosService
