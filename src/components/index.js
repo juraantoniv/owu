4d95a6090e475b98189a833fe5deadc7f27cf075
juaantoniv <juraantoniv@gmail.com>
2022-10-12 13:32:20 +0300

    y

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export  * from "./Header/Header"
-export  * from './Users/Users'
-export  * from './User/User'
-export  * from './Posts/Posts'
--- src/services/index.js
+++
@@ -1,3 +0,0 @@
-export * from './user.service'
-export * from './axios.service'
-export  * from './post.service'
